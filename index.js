335fb20b7e61572db293b6352094ed00f50c8da0
Kenneth <9827261u@gmail.com>
2026-04-29 18:21:17 -0600

    creando carpetas y arquiutecturas  del proyecto

--- index.js
+++ index.js
@@ -16,27 +16,6 @@ app.use(cors({
 
 app.use(express.json());
 
-// ==================== SUPABASE ====================
-
-
-// ==================== RUTAS ====================
-
-// Health check (IMPORTANTE para VPS)
-app.get("/", (req, res) => {
-  res.json({
-    message: "API funcionando 🚀",
-    server: os.hostname()
-  });
-});
-
-app.get("/status", (req, res) => {
-  res.json({
-    status: "OK",
-    server: os.hostname(),
-    timestamp: new Date().toISOString()
-  });
-});
-
 
 
 // ==================== MANEJO GLOBAL DE ERRORES ====================
