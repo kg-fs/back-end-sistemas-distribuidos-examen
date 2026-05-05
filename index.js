fc51a4434449499b2d60068e8bf560af24700b9c
Kenneth <9827261u@gmail.com>
2026-05-04 23:56:25 -0600

    modificando headers

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: "*",
   credentials: true  // Importante para cookies
 }));
 app.use(express.json());
